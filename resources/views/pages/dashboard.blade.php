c40f172cea54fd00ed8090ffcc05baff5867cd5f
VimalBharti <s.vimalbharti@gmail.com>
2021-06-30 09:19:36 +0530

    add logout

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -30,23 +30,23 @@
                         <img class="h-32 w-32 rounded-full shadow-xl" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
                         <div class="font-bold mt-3 pl-4">Vimal Bharti</div>
 
-                        <div class="social-links mt-20 grid grid-cols-2 gap-4">
-                            <a href="#" class="h-28 bg-indigo-500 bg-opacity-50 rounded-lg flex justify-center items-center">
+                        <div class="social-links mt-4 grid grid-cols-2 gap-4 h-2/4 overflow-scroll">
+                            <a href="#" class="h-full bg-indigo-500 bg-opacity-50 rounded-lg flex justify-center items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
                                 </svg>
                             </a>
-                            <a href="#" class="h-28 bg-red-500 bg-opacity-75 rounded-lg flex justify-center items-center">
+                            <a href="#" class="h-full bg-red-500 bg-opacity-75 rounded-lg flex justify-center items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10" />
                                 </svg>
                             </a>
-                            <a href="#" class="h-28 bg-black bg-opacity-75 rounded-lg flex justify-center items-center">
+                            <a href="#" class="h-full bg-black bg-opacity-75 rounded-lg flex justify-center items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
                                 </svg>
                             </a>
-                            <a href="#" class="h-28 bg-yellow-500 bg-opacity-50 rounded-lg flex justify-center items-center">
+                            <a href="#" class="h-full bg-yellow-500 bg-opacity-50 rounded-lg flex justify-center items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 8h6m-5 0a3 3 0 110 6H9l3 3m-3-6h6m6 1a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
--- resources/views/pages/sidebar.blade.php
+++ resources/views/pages/sidebar.blade.php
@@ -11,10 +11,19 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
         </button>
-        <button class="bg-gray-700 rounded-full p-2">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-            </svg>
-        </button>
+        
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button class="bg-gray-700 rounded-full p-2">
+            <a :href="route('logout')" 
+                onclick="event.preventDefault(); this.closest('form').submit();"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                </svg>
+            </a>
+            </button>
+        </form>
+        
     </div>
 </div>
